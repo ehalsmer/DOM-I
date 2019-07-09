981794948be021160e53ca8a71cdbae392d97a60
Eleasah Halsmer <eleasahjh@gmail.com>
2019-07-08 19:38:00 -0600

    Finished timer, added start button

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -8,27 +8,78 @@ const digits = document.querySelectorAll('.digit:not(#colon)');
 
 // };
 
-// countTen();
-const secondOnes = document.querySelector('#secondOnes');
+// Timer function:
 
-
+function timer(){
+    const secondOnes = document.querySelector('#secondOnes');
+const msHundreds = document.querySelector('#msHundreds');
+const msTens = document.querySelector('#msTens');
 
 let time = new Date();
 time.setTime(0);
 
-let interval = setInterval(timer, 1000);
+let interval = setInterval(seconds, 1000);
 
-function timer(){
+function seconds(){
     time.setTime(time.getTime()+1);
     // console.log(time.getTime());
 
     secondOnes.textContent = time.getTime();
     if (time.getTime() === 10){
-        // time.setTime(0);
         secondOnes.style.color = 'red';
         clearInterval(interval);
     }
-    // document.querySelector('#msHundreds').textContent = time.getTime()
-
 }
-timer();
+
+seconds();
+
+// 
+
+let time2 = new Date();
+time2.setTime(0);
+
+let interval2 = setInterval(msHund, 100);
+
+function msHund(){
+    time2.setTime(time2.getTime()+1);
+    msHundreds.textContent = Math.floor(time2.getTime() % 6);
+    // console.log(msHundreds.textContent);
+    if (time.getTime() === 10){
+        msHundreds.style.color = 'red';
+        clearInterval(interval2);
+        msHundreds.textContent = '0';
+    }
+}
+
+
+msHund();
+
+// 
+
+let time3 = new Date();
+time3.setTime(0);
+
+let interval3 = setInterval(msT, 10);
+
+function msT(){
+    time3.setTime(time3.getTime()+1);
+    msTens.textContent = Math.floor(time3.getTime() % 6);
+    if (time.getTime() === 10){
+        msTens.style.color = 'red';
+        clearInterval(interval3);
+        msTens.textContent = '0';
+    }
+}
+
+
+msT();
+}
+
+///////////////// Button event listener
+const btn = document.querySelector('button');
+btn.addEventListener('click', (event)=>{
+    timer();
+    btn.setAttribute("disabled", "disabled");
+    setTimeout(()=>btn.removeAttribute("disabled"), 10000)
+
+})
